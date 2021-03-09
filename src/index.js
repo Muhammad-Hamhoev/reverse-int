1fc62d1dc66d5628fd172ff018b9a568f99d66d5
Maga <ded.hamhoev@gmail.com>
2021-03-09 22:30:25 +0300

    done task reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+    console.log(n.toSring().reverse())
 }
+
+
+
+
+
+function reverse(n) {
+    console.log(n)
+    return n.toString().split("").reverse();
+}
+
+
+console.log(reverse(123456789));
